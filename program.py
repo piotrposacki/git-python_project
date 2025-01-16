6a26349c305ef00ca1a4a6c34c4fc51466aaff1a
Piotr Posacki <pposacki@wp.pl>
2025-01-16 15:25:52 +0100

    czytanie plików w folderze

--- program.py
+++ program.py
@@ -1,1 +1,9 @@
-print("zaczynamy")
+import os
+
+os.chdir("C:\\logs")
+print(os.getcwd())
+
+print(os.listdir())
+for file in os.listdir():
+    with open(file) as current:
+        print(current.readline())
